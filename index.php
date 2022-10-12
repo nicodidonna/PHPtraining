3545e12715377178657eaa0e2ff624a13927dc7e
Nico Didonna <nico_didonna@yahoo.it>
2022-10-12 10:26:30 +0200

    Bootstrap-italia inside

--- index.php
+++ index.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./bootstrap-italia/css/bootstrap-italia.min.css" />
     <title>PHP Inizio</title>
 </head>
 <body>
     <?php include 'header.html'?>
+    <div class="container">
     <h1>Form Esempio</h1>
 
     <form method="post" action="prova.php">
@@ -63,6 +65,10 @@
     
     <?php //funzione_esterna(); ?>
 
-
+    </div>
+    <script>
+    window.__PUBLIC_PATH__ = './bootstrap-italia/fonts';
+    </script>
+    <script src="./bootstrap-italia/js/bootstrap-italia.bundle.min.js"></script>
 </body>
 </html>
